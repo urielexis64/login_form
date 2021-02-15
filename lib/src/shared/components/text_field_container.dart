6524ecbf35a57716f999027a5b1516b2b79ec234
Uriel Aispuro <urielalexis64@gmail.com>
2021-02-14 20:28:17 -0700

    text field container widget

---
+++ lib/src/shared/components/text_field_container.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:login_form/constants.dart';
+
+class TextFieldContainer extends StatelessWidget {
+  final Widget child;
+
+  const TextFieldContainer({Key key, @required this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+      width: size.width * .8,
+      decoration: BoxDecoration(
+          color: kPrimaryLightColor, borderRadius: BorderRadius.circular(20)),
+      child: child,
+    );
+  }
+}
