723353fa73230e72cbac9d85ff866447b2cf2c6c
Uriel Aispuro <urielalexis64@gmail.com>
2021-02-14 20:32:41 -0700

    already account

---
+++ lib/src/shared/components/already_have_an_account.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:login_form/constants.dart';
+
+class AlreadyHaveAnAccountCheck extends StatelessWidget {
+  final bool login;
+  final Function press;
+  const AlreadyHaveAnAccountCheck({Key key, this.login, this.press})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          login ? 'Dont have an account? ' : 'Already have an account? ',
+          style: TextStyle(color: kPrimaryColor),
+        ),
+        GestureDetector(
+          onTap: press,
+          child: Text(
+            login ? 'Sign up' : 'Sign In',
+            style: TextStyle(color: kPrimaryColor, fontWeight: FontWeight.bold),
+          ),
+        )
+      ],
+    );
+  }
+}
