c58a3cfb64a7aa6232ca293d1bbd252bf79ed14c
Uriel Aispuro <urielalexis64@gmail.com>
2021-02-15 19:41:08 -0700

    Sig Up route

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:login_form/src/pages/home/home_page.dart';
 import 'package:login_form/src/pages/login/login_page.dart';
+import 'package:login_form/src/pages/signup/signup_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,8 +13,9 @@ class MyApp extends StatelessWidget {
       title: 'Login Forms',
       initialRoute: 'login',
       routes: {
-        'login': (BuildContext context) => LoginPage(),
         'home': (BuildContext context) => HomePage(),
+        'login': (BuildContext context) => LoginPage(),
+        'signup': (BuildContext context) => SignupPage(),
       },
     );
   }
---
+++ lib/src/pages/signup/signup_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:login_form/src/pages/signup/components/body.dart';
+
+class SignupPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(
+        child: Column(),
+      ),
+    );
+  }
+}
