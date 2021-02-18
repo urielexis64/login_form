87f918c18d7cead398d0609864f591fed8fe9467
Uriel Aispuro <urielalexis64@gmail.com>
2021-02-17 21:29:15 -0700

    Singleton Provider

--- lib/src/bloc/provider.dart
+++ lib/src/bloc/provider.dart
@@ -3,9 +3,19 @@ import 'package:login_form/src/bloc/login_bloc.dart';
 export 'package:login_form/src/bloc/login_bloc.dart';
 
 class Provider extends InheritedWidget {
+  static Provider _instance;
+
+  factory Provider({Key key, Widget child}) {
+    if (_instance == null) {
+      _instance = new Provider._internal(key: key, child: child);
+    }
+
+    return _instance;
+  }
+
   final loginBloc = LoginBloc();
 
-  Provider({Key key, Widget child}) : super(key: key, child: child);
+  Provider._internal({Key key, Widget child}) : super(key: key, child: child);
 
   @override
   bool updateShouldNotify(InheritedWidget oldWidget) => true;
