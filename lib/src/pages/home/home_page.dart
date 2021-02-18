9462b3a5c40d2dc587ee638914a174278069aa67
Uriel Aispuro <urielalexis64@gmail.com>
2021-02-17 21:30:06 -0700

    HomePage (email and password info)

--- lib/src/pages/home/home_page.dart
+++ lib/src/pages/home/home_page.dart
@@ -1,11 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:login_form/src/bloc/provider.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final bloc = Provider.of(context);
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Home Page'),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.logout),
+              onPressed: () => Navigator.pushReplacementNamed(context, 'login'))
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 22, horizontal: 20),
+        child: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: SvgPicture.asset(
+                'assets/icons/welcome.svg',
+                width: size.width * .8,
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text(
+                '${bloc.email}',
+                style: TextStyle(fontWeight: FontWeight.bold),
+                overflow: TextOverflow.ellipsis,
+              ),
+              trailing: Icon(Icons.edit),
+              onTap: () {},
+              tileColor: Colors.grey[100],
+              shape: StadiumBorder(),
+            ),
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.lock),
+              title: Text('${bloc.password}',
+                  style: TextStyle(fontWeight: FontWeight.w400)),
+              trailing: Icon(Icons.edit),
+              onTap: () {},
+              tileColor: Colors.grey[100],
+              shape: StadiumBorder(),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/src/pages/signup/signup_page.dart
+++ lib/src/pages/signup/signup_page.dart
@@ -5,9 +5,7 @@ class SignupPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Body(
-        child: Column(),
-      ),
+      body: Body(),
     );
   }
 }
