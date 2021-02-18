a5d913f3976839421941ad62228e367a2160c1c0
Uriel Aispuro <urielalexis64@gmail.com>
2021-02-17 18:34:15 -0700

    Provider widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:login_form/src/bloc/provider.dart';
 import 'package:login_form/src/pages/home/home_page.dart';
 import 'package:login_form/src/pages/login/login_page.dart';
 import 'package:login_form/src/pages/signup/signup_page.dart';
@@ -8,7 +9,8 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return Provider(
+        child: MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Login Forms',
       initialRoute: 'login',
@@ -17,6 +19,6 @@ class MyApp extends StatelessWidget {
         'login': (BuildContext context) => LoginPage(),
         'signup': (BuildContext context) => SignupPage(),
       },
-    );
+    ));
   }
 }
