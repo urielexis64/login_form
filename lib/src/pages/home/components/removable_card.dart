a0acd2b511d6d5f1f96964460de9ccc6bb30331a
Uriel Aispuro <urielalexis64@gmail.com>
2021-02-28 00:04:28 -0700

    Removable card widget

---
+++ lib/src/pages/home/components/removable_card.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+import 'package:login_form/src/models/product_model.dart';
+
+class RemovableCard extends StatelessWidget {
+  final ProductModel product;
+  final Function onRemove;
+  final Function onTap;
+
+  RemovableCard({
+    this.product,
+    this.onRemove,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Stack(children: [
+        Card(
+          clipBehavior: Clip.antiAlias,
+          child: Column(children: [
+            (product.urlImage == null)
+                ? Image.asset('assets/images/no-image.png')
+                : FadeInImage(
+                    placeholder: AssetImage('assets/images/jar-loading.gif'),
+                    image: NetworkImage(product.urlImage),
+                    height: 120,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+            Expanded(
+              child: Divider(),
+            ),
+            Text(
+              product.title,
+              overflow: TextOverflow.ellipsis,
+            ),
+            SizedBox(
+              height: 6,
+            )
+          ]),
+        ),
+        Positioned(
+          right: -14,
+          top: -14,
+          child: IconButton(
+            padding: EdgeInsetsDirectional.zero,
+            iconSize: 24,
+            color: Colors.redAccent,
+            icon: Icon(
+              Icons.remove_circle,
+            ),
+            onPressed: onRemove,
+            tooltip: 'Remove ${product.title}',
+          ),
+        )
+      ]),
+      onTap: onTap,
+    );
+  }
+}
