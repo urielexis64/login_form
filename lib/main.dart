b846200e6ad4293bbdcebcdcdb5f46ca258bb899
Uriel Aispuro <urielalexis64@gmail.com>
2021-03-03 17:15:03 -0700

    userPrefs implemented on routes

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,21 @@ import 'package:login_form/src/pages/home/home_page.dart';
 import 'package:login_form/src/pages/login/login_page.dart';
 import 'package:login_form/src/pages/product/product_page.dart';
 import 'package:login_form/src/pages/signup/signup_page.dart';
+import 'package:login_form/src/preferences/user_prefs.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = UserPrefs();
+  await prefs.initPrefs();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final prefs = UserPrefs();
+    print(prefs.token);
+
     return Provider(
         child: MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -20,7 +29,8 @@ class MyApp extends StatelessWidget {
         primaryColor: Constants.kPrimaryColor,
         accentColor: Constants.kPrimaryLightColor,
       ),
-      initialRoute: 'login',
+      //initialRoute: prefs.token != '' ? 'home' : 'login',
+      initialRoute: prefs.token == '' ? 'login' : 'home',
       routes: {
         'home': (BuildContext context) => HomePage(),
         'login': (BuildContext context) => LoginPage(),
