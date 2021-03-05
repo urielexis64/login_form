b7df926f77e85749bb5894acb3f620546d194966
Uriel Aispuro <urielalexis64@gmail.com>
2021-03-04 18:22:48 -0700

    authentication

--- lib/src/providers/products_provider.dart
+++ lib/src/providers/products_provider.dart
@@ -4,13 +4,15 @@ import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:login_form/src/models/product_model.dart';
 import 'package:http_parser/http_parser.dart';
+import 'package:login_form/src/preferences/user_prefs.dart';
 import 'package:mime_type/mime_type.dart';
 
 class ProductsProvider {
   final String _url = 'flutter-form-1db43-default-rtdb.firebaseio.com';
+  final _prefs = UserPrefs();
 
   Future<bool> createProduct(ProductModel product) async {
-    final uri = Uri.https(_url, '/products.json');
+    final uri = Uri.https(_url, '/products.json', {'auth': _prefs.token});
     final response = await http.post(uri, body: productModelToJson(product));
 
     final decodedData = json.decode(response.body);
@@ -19,7 +21,8 @@ class ProductsProvider {
   }
 
   Future<bool> updateProduct(ProductModel product) async {
-    final uri = Uri.https(_url, '/products/${product.id}.json');
+    final uri =
+        Uri.https(_url, '/products/${product.id}.json', {'auth': _prefs.token});
     final response = await http.put(uri, body: productModelToJson(product));
 
     final decodedData = json.decode(response.body);
@@ -27,13 +30,15 @@ class ProductsProvider {
   }
 
   Future<List<ProductModel>> loadProducts() async {
-    final uri = Uri.https(_url, '/products.json');
+    final uri = Uri.https(_url, '/products.json', {'auth': _prefs.token});
     final response = await http.get(uri);
 
     final Map<String, dynamic> decodedData = json.decode(response.body);
     final List<ProductModel> products = [];
 
     if (decodedData == null) return [];
+
+    if (decodedData['error'] != null) return [];
 
     decodedData.forEach((id, prod) {
       final prodTemp = ProductModel.fromJson(prod);
@@ -46,7 +51,7 @@ class ProductsProvider {
   }
 
   Future<int> removeProduct(String id) async {
-    final uri = Uri.https(_url, '/products/$id.json');
+    final uri = Uri.https(_url, '/products/$id.json', {'auth': _prefs.token});
     final response = await http.delete(uri);
 
     return 1;
