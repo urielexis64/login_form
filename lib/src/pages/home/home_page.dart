99f572d1776f1af7dbe283c882b5a41bcce8b823
Uriel Aispuro <urielalexis64@gmail.com>
2021-03-04 18:21:25 -0700

    RefreshIndicator widget and products bloc

--- lib/src/pages/home/home_page.dart
+++ lib/src/pages/home/home_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:login_form/src/bloc/provider.dart';
 import 'package:login_form/src/models/product_model.dart';
 import 'package:login_form/src/pages/home/components/removable_card.dart';
 import 'package:login_form/src/preferences/user_prefs.dart';
-import 'package:login_form/src/providers/products_provider.dart';
 import 'package:login_form/src/shared/components/custom_alert_dialog.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,11 +12,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final productsProvider = ProductsProvider();
-
   @override
   Widget build(BuildContext context) {
-    final bloc = Provider.of(context);
+    final productsBloc = Provider.productsBloc(context);
+    productsBloc.loadProducts();
 
     return Scaffold(
       appBar: AppBar(
@@ -31,18 +30,49 @@ class _HomePageState extends State<HomePage> {
               })
         ],
       ),
-      body: _createListView(),
+      body: RefreshIndicator(
+          color: Colors.deepPurple,
+          onRefresh: () async {
+            productsBloc.loadProducts();
+          },
+          child: _createListView(productsBloc)),
       floatingActionButton: _createButton(context),
     );
   }
 
-  Widget _createListView() {
-    return FutureBuilder(
-      future: productsProvider.loadProducts(),
+  Widget _createListView(ProductsBloc bloc) {
+    Size size = MediaQuery.of(context).size;
+
+    return StreamBuilder(
+      stream: bloc.productsStream,
       builder:
           (BuildContext context, AsyncSnapshot<List<ProductModel>> snapshot) {
         if (snapshot.hasData) {
           List<ProductModel> products = snapshot.data;
+          if (products.isEmpty) {
+            return Center(
+              child: SingleChildScrollView(
+                  physics: AlwaysScrollableScrollPhysics(),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SvgPicture.asset(
+                        'assets/icons/empty.svg',
+                        width: size.width * .8,
+                      ),
+                      SizedBox(
+                        height: 50,
+                      ),
+                      Text(
+                        'There\'s no products',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w300, fontSize: 20),
+                      ),
+                    ],
+                  )),
+            );
+          }
           return Padding(
             padding: const EdgeInsets.all(14.0),
             child: GridView.builder(
@@ -54,8 +84,8 @@ class _HomePageState extends State<HomePage> {
                   mainAxisSpacing: 6.0,
                 ),
                 itemCount: products.length,
-                itemBuilder: (context, index) =>
-                    _createItem(context, products[index], products, index)),
+                itemBuilder: (context, index) => _createItem(
+                    context, bloc, products[index], products, index)),
           );
         } else {
           return Center(child: CircularProgressIndicator());
@@ -64,12 +94,12 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget _createItem(
-      BuildContext context, ProductModel product, List products, int index) {
+  Widget _createItem(BuildContext context, ProductsBloc bloc,
+      ProductModel product, List products, int index) {
     return RemovableCard(
         product: product,
         onRemove: () {
-          _removeItem(product, products, index);
+          _removeItem(product, bloc, products, index);
         },
         onTap: () =>
             Navigator.pushNamed(context, 'product', arguments: product));
@@ -90,7 +120,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   Future<void> _removeItem(
-      ProductModel product, List products, int index) async {
+      ProductModel product, ProductsBloc bloc, List products, int index) async {
     return showDialog<void>(
       context: context,
       barrierDismissible: false,
@@ -98,12 +128,10 @@ class _HomePageState extends State<HomePage> {
         return CustomAlertDialog(
           title: 'Remove item',
           onConfirmed: () {
-            productsProvider
-                .removeProduct(product.id)
-                .then((value) => setState(() {
-                      products.removeAt(index);
-                      Navigator.pop(context);
-                    }));
+            bloc.removeProduct(product.id).then((value) => setState(() {
+                  products.removeAt(index);
+                  Navigator.pop(context);
+                }));
           },
           contentWidgets: [
             Text("Are you sure you want to remove '${product.title}'?"),
